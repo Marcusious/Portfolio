a6233059fd12bf7b83900901478178935e744e1c
Marcusious <hoganmarcus092@gmail.com>
2024-09-26 03:55:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function sendMail(){
-    let parms ={
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        number: document.getElementById('subject').value,
-        message: document.getElementById('message').value,
-    }
 
-    emailjs.send('service_b8jj8ql','template_zm7b9jb',parms).then(alert('Email Sent!!'))
-}
